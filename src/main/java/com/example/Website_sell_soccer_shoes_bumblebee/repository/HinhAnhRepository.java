a5f186de003378a0330925972b7d178f91076164
huyenhtt02 <huyenhttph25539@fpt.edu.vn>
2023-10-06 21:46:30 +0700

    hinh_anh

--- src/main/java/com/example/Website_sell_soccer_shoes_bumblebee/repository/HinhAnhRepository.java
+++ src/main/java/com/example/Website_sell_soccer_shoes_bumblebee/repository/HinhAnhRepository.java
@@ -7,10 +7,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
-
+@Repository
 public interface HinhAnhRepository extends JpaRepository<HinhAnh, UUID> {
 
     @Query(value = "select h from HinhAnh h where h.id = ?1")
